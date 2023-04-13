34c708ce0e0d78b640006abc8e0268e09d4e611f
alotofmilk <munzimelo@ewhain.net>
2023-04-13 14:57:55 +0900

    create 기능까지 구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'reviews'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+
 ]
